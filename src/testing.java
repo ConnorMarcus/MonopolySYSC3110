3de07b6a06d0dc693995e797d4ef05653635eaf0
vahid <vahidforoughi@cmail.carleton.ca>
2021-10-12 11:48:30 -0400

    trying it out

--- src/testing.java
+++ src/testing.java
@@ -1,6 +1,6 @@
 public class testing {
     public static void main(String[] args) {
         System.out.println("testing");
-        System.out.println("testing vahid ");
+        System.out.println("testing vahid");
     }
 }
