e6f972f33b0e42a92a9249a3c96fd20c7654c2f8
Connor <connormarcus@bell.net>
2021-10-12 21:05:14 -0400

    commit changes before pull

--- src/testing.java
+++ src/testing.java
@@ -3,9 +3,8 @@ import java.util.Map;
 
 public class testing {
     public static void main(String[] args) {
-        System.out.println("testing");
-        System.out.println("testing vahid");
-        CommandList c = new CommandList(new HashMap<>(Map.of("HELP", () -> System.out.println("List of Available Commands: roll, buy, pass, help"))));
-        c.executeCommand("help");
+        Dice d = new Dice();
+        CommandList c = new CommandList(new HashMap<>(Map.of("HELP", () -> System.out.println("List of Available Commands: roll, buy, pass, help"), "ROLL", d::rollDice)));
+        c.executeCommand("roll");
     }
 }
