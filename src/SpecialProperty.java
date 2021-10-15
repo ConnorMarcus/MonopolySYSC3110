51e012f9a77bcd5feefe6a0129cf7f92d3a2ad81
vahid <vahidforoughi@cmail.carleton.ca>
2021-10-15 17:26:07 -0400

    Added methods in SpecialProperty.java - vahid

--- src/SpecialProperty.java
+++ src/SpecialProperty.java
@@ -11,7 +11,20 @@ public class SpecialProperty extends Property {
         this.action = action;
     }
 
-    public void railroad(){
-
+    /**
+     * User must pay an income tax when they land on this property
+     *
+     * @return int, 200 dollars the user must pay
+     */
+    public int incomeTax(){
+        return 200;
+    }
+    /**
+     * User must pay a super tax when they land on this property
+     *
+     * @return int, 100 dollars the user must pay
+     */
+    public int superTax(){
+        return 100;
     }
 }
