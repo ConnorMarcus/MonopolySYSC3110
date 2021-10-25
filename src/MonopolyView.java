488736cb84fe6dbf1816a88516c3503783ceaff2
Connor <connormarcus@bell.net>
2021-10-25 15:09:09 -0400

    Added Code for Milestone 2

--- src/MonopolyView.java
+++ src/MonopolyView.java
@@ -6,15 +6,6 @@ public class MonopolyView extends JFrame {
     public MonopolyView(String title) {
         super(title);
         boardPanel = new BoardPanel(new MonopolyBoard());
-        Player testPlayer = new Player("1", "images/player1.png");
-        Player testPlayer2 = new Player("2", "images/player2.png");
-        Player testPlayer3 = new Player("3", "images/player3.png");
-        Player testPlayer4 = new Player("4", "images/player4.png");
-        boardPanel.addPlayerLabel(testPlayer);
-        boardPanel.addPlayerLabel(testPlayer2);
-        boardPanel.addPlayerLabel(testPlayer3);
-        boardPanel.addPlayerLabel(testPlayer4);
-
         this.add(boardPanel);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
